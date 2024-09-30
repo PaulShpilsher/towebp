40e21d242c57cfa3451222cdc9f671e381d5178e
Paul Shpilsher <paul.shpilsher@gmail.com>
2024-09-30 11:25:45 +0300

    initial checkin

---
+++ go.mod
@@ -0,0 +1,5 @@
+module towebp
+
+go 1.23.1
+
+require github.com/sizeofint/webpanimation v0.0.0-20210809145948-1d2b32119882
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"image/gif"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/sizeofint/webpanimation"
+)
+
+func main() {
+	var buf bytes.Buffer
+	gifFile, err := os.Open("animated_gif.gif")
+	if err != nil {
+		log.Fatal(err)
+	}
+	gif, err := gif.DecodeAll(gifFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	webpanim := webpanimation.NewWebpAnimation(gif.Config.Width, gif.Config.Height, gif.LoopCount)
+	webpanim.WebPAnimEncoderOptions.SetKmin(9)
+	webpanim.WebPAnimEncoderOptions.SetKmax(17)
+	defer webpanim.ReleaseMemory() // dont forget call this or you will have memory leaks
+	webpConfig := webpanimation.NewWebpConfig()
+	webpConfig.SetLossless(0)
+
+	timeline := 0
+
+	for i, img := range gif.Image {
+
+		err = webpanim.AddFrame(img, timeline, webpConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+		timeline += gif.Delay[i] * 10
+	}
+	err = webpanim.AddFrame(nil, timeline, webpConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = webpanim.Encode(&buf) // encode animation and write result bytes in buffer
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ioutil.WriteFile("animated_gif.webp", buf.Bytes(), 0777)
+}
